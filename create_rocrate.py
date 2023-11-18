a4f52cd4593ed9cac6ba7bad898ca97a5e5b9608
RenskeW <renskedewit1998@gmail.com>
2023-11-19 00:12:29 +0100

    updated ROCrate manifest

--- create_rocrate.py
+++ create_rocrate.py
@@ -10,18 +10,22 @@ readme = crate.add_file("./README.md", properties = {
 
 scenario1 = crate.add_dataset("./scenario1", properties = {
     "name": "scenario1",
+    "description": "Analyze representation of CWL metadata fields and human agent"
 })
 
 scenario2 = crate.add_dataset("./scenario2", properties = {
-    "name": "scenario2_a",
+    "name": "scenario2",
+    "description": "Analyze representation of SoftwareRequirement",
 })
 
 scenario3 = crate.add_dataset("./scenario3", properties = {
     "name": "scenario3",
+    "description": "Analyze representation of DockerRequirement",
 })
 
 scenario4 = crate.add_dataset("./scenario4", properties = {
-    "name": "scenario4_a",
+    "name": "scenario4",
+    "description": "Analyze representation of input parameter values and ResourceRequirement",
 })
 
 renske_id = "https://orcid.org/0000-0003-0902-0086"
@@ -38,5 +42,3 @@ michael = crate.add(Person(crate, michael_id, properties = {
 readme["author"] = [renske, michael]
 
 crate.write("./analysis_rocrate")
-
-crate.write_zip("./analysis_rocrate.zip")
